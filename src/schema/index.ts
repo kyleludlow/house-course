b629918a8dab7144c6f4a209422031c5a891ec98
Kyle Ludlow <kyleludlow@gmail.com>
2021-07-04 11:52:09 -0700

    course complete

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,10 @@
-// import { buildSchemaSync } from "type-graphql";
-// import { ImageResolver } from "./image";
-// import { HouseResolver } from "./house";
-// import { authChecker } from "./auth";
+import { buildSchemaSync, Query, Resolver } from "type-graphql";
+import { ImageResolver } from "./image";
+import { HouseResolver } from "./house";
+import { authChecker } from "./auth";
 
-export {};
+export const schema = buildSchemaSync({
+  resolvers: [ImageResolver, HouseResolver],
+  emitSchemaFile: process.env.NODE_ENV === "development",
+  authChecker,
+});
--- src/utils/useLocalState.ts
+++ src/utils/useLocalState.ts
@@ -1,3 +1,23 @@
-// import { useState, useEffect } from "react";
+import { valueFromAST } from "graphql";
+import { useState, useEffect } from "react";
 
-export {};
+export function useLocalState<S = undefined>(key: string, initial: S) {
+  const [value, setValue] = useState<S>(() => {
+    // check if server rendering or not
+    if (typeof window !== "undefined" && window.localStorage) {
+      const saved = window.localStorage.getItem(key);
+      if (saved) {
+        return JSON.parse(saved);
+      }
+    }
+    return initial;
+  });
+
+  useEffect(() => {
+    if (window.localStorage) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value]);
+
+  return [value, setValue] as [typeof value, typeof setValue];
+}
